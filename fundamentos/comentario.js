f04cb9c7f706cab686b811112fefb90dfddaac58
kauesalvio <kauefsalvio@gmail.com>
2021-07-07 20:20:00 -0300

    feat: introdução de variaveis

---
+++ fundamentos/comentario.js
@@ -0,0 +1,7 @@
+//Comentario de uma linha
+console.log("Comentário de uma linha.")
+
+/*
+    Comentário de
+    múltiplas linhas
+*/
---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,16 @@
+let qualquer = "Legal"
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evitar nomes genéricos e siglas
+let valor = 6
+console.log(valor)
+
+let numero = 5
+console.log(numero)
+
+let ppp = false;
