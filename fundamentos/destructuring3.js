aa0128a90f9695c55b8f19c4c06d67a61c3d5d3b
kauesalvio <kaue.salvio@ambevtech.com.br>
2021-07-15 21:13:43 -0300

    feat: aprendendo destructuring e operadores aritmeticos

---
+++ fundamentos/destructuring3.js
@@ -0,0 +1,12 @@
+function rand({ min = 0, max = 1000} = {}){
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+const obj = {
+    min: 50,
+    max: 100
+}
+
+console.log(rand(obj))
+console.log(rand({}))
